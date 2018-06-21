44f7a01ce66f59dea9f587416d64663d022a0524
Marc Brinkmann <marc@49nord.de>
2018-06-21 19:04:50 +0200

    Added top-level docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! Process guard
+//!
+//! A process guard takes ownership of a `process::Child` and gently or forcefully kills it upon,
+//! prevent the process from running on. Example:
+//!
+//! ```rust
+//! use process_guard::ProcessGuard;
+//! use std::process;
+//!
+//! fn insomnia() {
+//!     let cmd = process::Command::new("sleep").arg("120");
+//!     let pg = ProcessGuard::spawn(cmd);
+//!
+//!     // a two-minute sleep process has been started, which will be killed as soon as this
+//!     // function returns
+//! }
+//! ```
+
 extern crate nix;
 extern crate ticktock;
 
